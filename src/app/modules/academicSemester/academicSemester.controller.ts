e068bae15e945a8fe015f53e26f99b6261202b63
unknown <tobiburrohman2@gmail.com>
2023-07-17 21:19:17 +0600

    14-4: Implement Pagination using page , limit and skip

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -4,6 +4,8 @@ import catchAsync from '../../../shared/catchAsync';
 import httpStatus from 'http-status';
 import { IAcademicSemester } from './academicSemester.interface';
 import sendResponse from '../../../shared/sendResponse';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination.constant';
 
 const createSemester = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -23,6 +25,26 @@ const createSemester = catchAsync(
   }
 );
 
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions = pick(req.query, paginationFields);
+    const result = await AcademicSemesterService.getAllSemesters(
+      paginationOptions
+    );
+
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'successfully got all semesters',
+      meta: result.meta,
+      data: result.data,
+    });
+
+    next();
+  }
+);
+
 export const AcademicSemesterController = {
   createSemester,
+  getAllSemesters,
 };
