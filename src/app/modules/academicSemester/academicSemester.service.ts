9ace0af4abb8ba0ca0ff8fdd70319042d46cc4c3
unknown <tobiburrohman2@gmail.com>
2023-07-17 22:36:19 +0600

    14-5: Implement Dynamic Sorting

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -5,6 +5,8 @@ import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicsSemester.model';
 import { IPaginationOptions } from '../../../interfaces/pagination';
 import { IGenericResponse } from '../../../interfaces/common';
+import { paginationHelpers } from '../../../helpers/pagination.helpers';
+import { SortOrder } from 'mongoose';
 
 const createSemester = async (
   payload: IAcademicSemester
@@ -19,8 +21,14 @@ const createSemester = async (
 const getAllSemesters = async (
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
-  const { page = 1, limit = 10 } = paginationOptions;
-  const skip = (page - 1) * limit;
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
 
   const result = await AcademicSemester.find().sort().skip(skip).limit(limit);
 
